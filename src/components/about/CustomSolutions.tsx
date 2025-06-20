fd4482a81ce95fb3bbf1bdc9ebf1ce70a33a9c98
connor-kfitz <connorkfitzsimmons@gmail.com>
2025-06-19 21:17:02 -0400

    Fixed Custom Solutions Design Issue

--- src/components/about/CustomSolutions.tsx
+++ src/components/about/CustomSolutions.tsx
@@ -3,8 +3,8 @@ import { Button } from "../ui/button";
 export default function CustomSolutions() {
   return (
     <section className="mb-[48px] px-[24px] sm:px-[48px]">
-      <div className="flex justify-center items-center xl-block rounded-[24px] max-w-[1344px] min-h-[560px] md:min-h-[871px] mx-auto xl:pt-[217px] bg-custom-solutions xl:pr-[141px]">
-        <div className="xl:ml-auto max-w-[465px] px-[48px]">
+      <div className="flex xl:block justify-center items-center xl-block rounded-[24px] max-w-[1344px] min-h-[560px] md:min-h-[871px] mx-auto xl:pt-[217px] bg-custom-solutions xl:pr-[141px]">
+        <div className="xl:ml-auto max-w-[465px] px-[48px] xl:px-0">
           <h2 className="text-d2 text-gray-900 font-bold mb-4">Custom solutions for your drug therapies</h2>
           <p className="text-b6 text-gray-600 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
           <Button variant="primary" size="primaryDefault" className="text-md font-medium">Custome your solution</Button>
