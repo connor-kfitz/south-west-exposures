d4df97dc8e61d11c79e6943b96279585a5967cea
connor-kfitz <connorkfitzsimmons@gmail.com>
2025-06-21 15:22:45 -0400

    Updated Compliance Tile BG

--- src/components/about/Compliance.tsx
+++ src/components/about/Compliance.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
  
 export default function Compliance() {
   return (
-    <section className="w-full mb-[96px]">
+    <section className="w-full pb-[96px]">
       <div className="max-w-[1440px] mx-auto flex flex-col w-full lg:flex-row">
-        <div className="flex flex-col justify-center items-center w-1/2 basis-1/2 px-6 min-h-[350px] lg:min-h-[479px] w-full">
+        <div className="flex flex-col justify-center items-center w-1/2 basis-1/2 px-6 min-h-[350px] lg:min-h-[479px] w-full bg-white">
           <div className="lg:max-w-[465px]">
             <h2 className="text-h1 text-gray-900 font-semibold mb-4">Regulatory compliance and oversight</h2>
             <p className="text-b6 text-gray-600">The SWE team has experience working with both the CNSC and NRC. SWE provides customizable solutions and package certification services that conform to IAEA, DOT and IATA regulations.</p>
