c118d3f5bd316d55bc6acc5cb9862ab7f8605f15
connor-kfitz <connorkfitzsimmons@gmail.com>
2026-01-19 22:14:30 -0500

    Deployment Fix

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -8,11 +8,7 @@ export const revalidate = 0;
 export async function generateMetadata({ params }: {params: Promise<{ productId: string }>}): Promise<Metadata> {
   const { productId } = await params
 
-  const cookieStore = await cookies();
-
-  const product = await fetch(`${process.env.DOMAIN_NAME}/api/admin/products/get/${productId}`, {
-    headers: { Cookie: cookieStore.toString() }
-  }).then((res) => res.json())
+  const product = await fetch(`${process.env.DOMAIN_NAME}/api/admin/products/get/${productId}`).then((res) => res.json())
 
   return {
     title: product.name,
